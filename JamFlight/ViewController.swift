9fcfa218911f83fe6dbad6fdd7c81da3a9e365b3
muhammadtafanirabbani <ratafani@gmail.com>
2019-07-08 18:20:40 +0700

    Initial Commit

---
+++ JamFlight/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JamFlight
+//
+//  Created by Muhammad Tafani Rabbani on 08/07/19.
+//  Copyright © 2019 Muhammad Tafani Rabbani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
