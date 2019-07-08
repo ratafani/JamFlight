e6194fe388127e02c7a2bed5cea786229785f8f7
muhammadtafanirabbani <ratafani@gmail.com>
2019-07-08 19:23:28 +0700

    first init

--- JamFlight/ViewController.swift
+++ JamFlight/ViewController.swift
@@ -7,14 +7,58 @@
 //
 
 import UIKit
+import AudioKit
 
+struct chordData {
+    var chord : String
+    var octav : Int
+    var cent : Int
+}
 class ViewController: UIViewController {
-
+    let mic = AKMicrophone()
+    var tracker = AKFrequencyTracker()
+    var silence: AKBooster!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tracker = AKFrequencyTracker(mic)
+        silence = AKBooster(tracker, gain: 0)
+        AKSettings.audioInputEnabled = true
+        
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        do{
+            try AudioKit.stop()
+            AudioKit.output = silence
+            try AudioKit.start()
+            
+            _ = Timer.scheduledTimer(timeInterval: 0.1, target:self, selector:  #selector(record), userInfo: nil, repeats: true)
+            
+        }catch{
+            print(error)
+        }
+    }
+    
+    @objc func record(){
+        let chord = freqToChord(f: tracker.frequency)
+        print(chord)
+    }
+    
+    let chordArr : [String] = ["A","A#","B","C","C#","D","D#","E","F","F#","G","G#"]
+    func freqToChord(f:Double)->chordData{
+        let f1 = log2(f/440 ) * 12
+        let cent = Int(f1*100)%100
+        let octav = 4 + (Int(log2(f/440).rounded()))
+        let move = Int(f1.rounded()) % 12
+        var chord = ""
+        if move < 0{
+            chord = chordArr[12+move]
+        }else{
+            chord = chordArr[move]
+        }
+        let cData:chordData = chordData(chord: chord, octav: octav, cent: cent)
+        return cData
+    }
 }
 
